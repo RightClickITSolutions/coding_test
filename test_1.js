5797ff5a420dd3eb6a6d18b3b0b3ca449839bd47
kabeerkhaleed <khalidgombawa@gmail.com>
2023-02-09 21:15:26 +0100

    Developer Test Solutions

---
+++ test_1.js
@@ -0,0 +1,30 @@
+function calculateStorage(size) {
+    // Function to calculate the actual storage capacity of hard drive
+     const oneGB = 1000;
+     // Assume that 1 GB = 1000 MB
+     const oneTB = 1000;
+    // Assume that 1 TB = 1000 GB
+     let value = parseFloat(size.match(/\d+\.\d+|\d+/g));
+     let unit = size.match(/[a-zA-Z]+/g)[0];
+    // Split the input string to extract the value and unit
+     
+     if (unit === 'TB') {
+       value = value * oneTB * oneGB;
+     } else if (unit === 'GB') {
+       value = value * oneGB;
+     } else if (unit === 'MB') {
+     // Converts the value to MB
+     } else {
+       console.log("Invalid unit. Please enter the size in format 'value unit' (e.g. 1 TB, 500 GB, 100 MB)");
+       return;
+     }
+     
+     console.log(`Actual storage capacity: ${value} MB`);
+   }
+   
+   const size = prompt("Enter the size of the hard drive specified by the manufacturer (e.g. 1 TB, 500 GB, 100 MB)");
+   // Gets input from user
+   
+   calculateStorage(size);
+   // Calls the function to calculate and outputs the storage capacity
+   
---
+++ test_3.0.py
@@ -0,0 +1,26 @@
+import csv
+from collections import defaultdict
+
+def goat_awards(csv_file):
+    players = defaultdict(lambda: defaultdict(int))
+    
+    with open(csv_file, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            name = row['name']
+            players[name]['points'] += int(row['points'])
+            players[name]['assists'] += int(row['assists'])
+            players[name]['rebounds'] += int(row['rebounds'])
+            players[name]['blocks'] += int(row['blocks'])
+            
+    most_points = max(players, key=lambda x: players[x]['points'])
+    most_assists = max(players, key=lambda x: players[x]['assists'])
+    most_rebounds = max(players, key=lambda x: players[x]['rebounds'])
+    most_blocks = max(players, key=lambda x: players[x]['blocks'])
+    
+    return {
+        'most_points': [most_points, players[most_points]['points']],
+        'most_assists': [most_assists, players[most_assists]['assists']],
+        'most_rebounds': [most_rebounds, players[most_rebounds]['rebounds']],
+        'most_blocks': [most_blocks, players[most_blocks]['blocks']]
+    }
