873ec97af4e57be007bb41be0e7e7abcb64a22b2
Lateef Wasiu Olalekan <olaokunolalekan@gmail.com>
2023-02-18 16:00:00 +0100

    initial commit

--- test_2.js
+++ test_2.js
@@ -6,3 +6,22 @@ A milk carton can hold 3.78 liters of milk. Each morning, a dairy farm ships car
 	- Outputs the profit for producing milk.
 
  */
+let oneLiter = 250;
+let profitPerEachCarton = 180;
+const totalAmountOfMilkProduced = prompt(
+  "Enter the total amount of milk produced in the morning (in litres)"
+);
+
+const numberOfMilkCartonNeeded = (totalAmountOfMilkProduced / 3.78).toFixed(0);
+
+const costOfMilkProduction = totalAmountOfMilkProduced * oneLiter;
+
+const profitForMilkProduced = numberOfMilkCartonNeeded * profitPerEachCarton;
+
+console.log(
+  `The number of milk cartons needed to hold milk is ${numberOfMilkCartonNeeded} cartons`
+);
+console.log(`The cost of the milk production is #${costOfMilkProduction}`);
+console.log(
+  `The profit of the milk production is #${profitForMilkProduced} cartons`
+);
